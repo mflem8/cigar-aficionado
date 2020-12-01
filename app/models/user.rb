9875950552439acd1a82a116484ff82ae26a0912
mflem8 <matthewfleming8@gmail.com>
2020-11-30 20:39:15 -0500

    adds has_secure_password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_cigars, through: :reviews, source: :cigar
-
     has_many :cigars
+    has_secure_password
+    validates :username, uniqueness: true, presence: true
 end
