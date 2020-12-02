3d5d0e07801c7c4e3d0a72575e2a2a7dc54b94ad
mflem8 <matthewfleming8@gmail.com>
2020-12-02 13:22:33 -0500

    add flash errors for sessions login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,20 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end
 
+    def new
 
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Invalid login credentials. Please try again."
+            redirect_to login_path
+        end
+    end
 
     
 end
