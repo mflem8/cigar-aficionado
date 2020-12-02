23a0b083d1f22935683344f20f3a61e9c695d9e3
mflem8 <matthewfleming8@gmail.com>
2020-12-02 13:56:26 -0500

    updates new action to accomodate nested resources

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+        @cigar = Cigar.find_by_id(params[:cigar_id])
+        @review = @cigar.build_review
+    end
+
 end
