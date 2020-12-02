a57808785fab0fcedbc1cd7d8f11598de1be51bc
mflem8 <matthewfleming8@gmail.com>
2020-12-02 13:42:14 -0500

    adds index action to controllers

--- app/controllers/cigars_controller.rb
+++ app/controllers/cigars_controller.rb
@@ -1,8 +1,22 @@
 class CigarsController < ApplicationController
 
+    def index
+        @cigars = Cigar.all
+    end
+    
     def new
         @cigar = Cigar.new
         @cigar.build_brand
+    end
+
+    def create
+        @cigar = Cigar.new(cigar_params)
+        @cigar.user_id = session[:user_id]
+        if @cigar.save!
+            redirect_to cigar_path(@cigar)
+        else
+            render :new
+        end
     end
 
     private
