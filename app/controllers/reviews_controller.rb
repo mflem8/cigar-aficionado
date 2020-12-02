8d4626e590620aa446f77fe84f80520382c6d89d
mflem8 <matthewfleming8@gmail.com>
2020-12-02 14:21:39 -0500

    building out reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,31 @@
 class ReviewsController < ApplicationController
 
     def index
-
+        @reviews = Review.all
     end
 
     def new
         @cigar = Cigar.find_by_id(params[:cigar_id])
-        @review = @cigar.build_review
+        @review = @cigar.reviews.build
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end
+    end
+
+    def show
+
+    end
+
+    private 
+    
+    def review_params
+        params.require(:review).permit(:cigar_id, :summary, :matches, :title)
     end
 
 end
