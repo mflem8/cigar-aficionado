4cd70907963a1bd96adb3222d6738ed109c96963
mflem8 <matthewfleming8@gmail.com>
2020-12-03 19:18:24 -0500

    update actions to implement current_user method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,23 @@
 class ReviewsController < ApplicationController
 
     def index
-        @reviews = Review.all
+        if @cigar = Cigar.find_by_id(params[:cigar_id])
+            @reviews = @cigar.reviews
+        else
+            @reviews = Review.all
+        end
     end
 
     def new
-        @cigar = Cigar.find_by_id(params[:cigar_id])
-        @review = @cigar.reviews.build
+        if @cigar = Cigar.find_by_id(params[:cigar_id])
+            @review = @cigar.reviews.build
+        else
+            @review = Review.new 
+        end
     end
 
     def create
-        @review = Review.new(review_params)
+        @review = current_user.reviews.build(review_params)
         if @review.save
             redirect_to review_path(@review)
         else
@@ -19,7 +26,7 @@ class ReviewsController < ApplicationController
     end
 
     def show
-
+        @review = Review.find_by_id(params[:id])
     end
 
     private 
