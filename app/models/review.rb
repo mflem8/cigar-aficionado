54cbc33c90bfa2e65c15d1dfe77fb8ec1c3baa86
mflem8 <matthewfleming8@gmail.com>
2020-12-04 14:28:46 -0500

    added custom validation for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   belongs_to :cigar
 
   validates :title, presence: true
-  validates :matches, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6} 
+  validates :matches, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
+  
+  validates :cigar_id, uniqueness: { scope: :user }
 end
