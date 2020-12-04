2ce15319cd86700753f3de8bd0882853a487fc17
mflem8 <matthewfleming8@gmail.com>
2020-12-03 19:47:52 -0500

    added model validation

--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -4,4 +4,12 @@ class Cigar < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
   accepts_nested_attributes_for :brand
+
+  validates :brand, presence: true
+
+  def brand_attributes=(attributes)
+    brand = Brand.find_or_create_by(attributes) if
+    !attributes['name'].empty?
+  end
+
 end
