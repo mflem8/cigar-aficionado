79d4e4ff364330cd5de70956e1626ca92b8a566d
mflem8 <matthewfleming8@gmail.com>
2020-12-03 19:18:40 -0500

    added model validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :cigar
+
+  validates :title, presence: true
+  validates :matches, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6} 
 end
