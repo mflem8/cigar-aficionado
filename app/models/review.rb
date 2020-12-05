0ea8fe0278267599c07001ced710b972e68b4430
mflem8 <matthewfleming8@gmail.com>
2020-12-04 21:40:17 -0500

    fix errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :matches, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
   
-  validates :cigar_id, uniqueness: { scope: :user }
+  validates :cigar_id, uniqueness: { scope: :user, message: "has already been reviewed by you" }
 end
