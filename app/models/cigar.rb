39c9af47c98ede3524518874858d7635d0fedc70
mflem8 <matthewfleming8@gmail.com>
2020-12-04 22:49:50 -0500

    add helper methods to model

--- app/models/cigar.rb
+++ app/models/cigar.rb
@@ -3,13 +3,27 @@ class Cigar < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
-  accepts_nested_attributes_for :brand
 
-  validates :brand, presence: true
+  validates :style, presence: true
+  validate :not_a_duplicate
 
   def brand_attributes=(attributes)
-    brand = Brand.find_or_create_by(attributes) if
-    !attributes['name'].empty?
+    brand = Brand.find_or_create_by(attributes) if !attributes['name'].empty?
+  end
+
+  def not_a_duplicate
+    cigar = Cigar.find_by(style: style, brand_id: brand_id)
+    if !!cigar && cigar != self
+      errors.add(:style, 'has already been added for that brand')
+    end
+  end
+
+  def brand_and_style
+    "#{brand.name} - #{style}"
+  end
+
+  def self.atoz
+    order(:brand)
   end
 
 end
