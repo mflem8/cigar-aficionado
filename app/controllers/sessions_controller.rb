65a49f06f1f5ea86768f60e2f3cbb4708c289f7e
mflem8 <matthewfleming8@gmail.com>
2020-12-05 12:08:36 -0500

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
     def index
-
     end
 
     def destroy
@@ -10,17 +9,23 @@ class SessionsController < ApplicationController
     end
 
     def new
-
     end
 
     def create
-        @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(params[:user][:password])
+
+        if params[:provider] == 'google_oauth2'
+            @user = User.create_by_google_omniauth(auth)
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            flash[:error] = "Invalid login credentials. Please try again."
-            redirect_to login_path
+            @user = User.find_by(username: params[:user][:username])
+            if @user && @user.authenticate(password: params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else
+                flash[:error] = "Invalid login credentials. Please try again."
+                redirect_to login_path
+            end
         end
     end
 
